db08641b1ae9f56cd47485063993312b63978ed2
robotwholearned <robotwholearned88@gmail.com>
2014-04-02 11:21:11 -0600

    testing building from yaml

--- config.rb
+++ config.rb
@@ -15,6 +15,8 @@ activate :blog do |blog|
   blog.sources = "blog/{year}-{month}-{day}-{title}.html"
   blog.tag_template = "tag.html"
   blog.calendar_template = "calendar.html"
+  #blog.sources = "blog/{article-number}.html"
+  #blog.permalink = "/{article-number}.html"
 
   # Enable pagination
    blog.paginate = true
@@ -24,9 +26,21 @@ end
 
 page "/feed.xml", layout: false
 
-["tom", "dick", "harry"].each do |name|
-  proxy "/about/#{name}.html", "/about/template.html", :locals => { :person_name => name }, :ignore => true
+# ["tom", "dick", "harry"].each do |name|
+#   proxy "/about/#{name}.html", "/about/template.html", :locals => { :person_name => name }, :ignore => true
+# end
+
+# data.blog_roll.comics.each do |comic|
+#   proxy "", "", :locals => {:filename => filename}
+# end
+data.people.each do |fruit|
+  proxy "/#{fruit.name}.html", "about/template.html", :locals => {:name => fruit.name, :price => fruit.price}, :ignore =>true
 end
+
+# Assumes the file source/about/template.html.erb exists
+# (ruby command to get all the images).each do |filename, index|
+#   proxy "/#{index}.html", "/blog/template.html", :locals => { :filename => filename }
+# end
 
 # Reload the browser automatically whenever files change
 activate :livereload
